b67d4b62c4fb2eaa763b34e20c42a0ec841f29ee
Diptanshu <sinha.diptanshu10@gmail.com>
2022-01-21 15:52:31 +0530

    Intervalometer aesthetics

--- Intervalometer.py
+++ Intervalometer.py
@@ -2,13 +2,16 @@ from libsonyapi.camera import Camera
 from libsonyapi.actions import Actions
 import requests 
 import time
-
+print("Connecting to camera")
 camera = Camera()  # create camera instance
+print("Connected to camera!!")
 camera_info = camera.info() # get camera camera_info
-delay_time=int(input("Enter delay time between photos"))
-num_shots=int(input("Enter number of photos to take"))
-start_time=int(input("Enter start timer"))
+delay_time=int(input("Enter delay time between photos:-"))
+num_shots=int(input("Enter number of photos to take:-"))
+start_time=int(input("Enter start timer:-"))
 time.sleep(start_time)
 for shot_no in range(num_shots):
     camera.do(Actions.actTakePicture)
+    print(shot_no+1+ " photo taken out of "+ num_shots)
     time.sleep(delay_time)
+print("Done")
